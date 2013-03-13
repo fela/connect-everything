91540f96a83a73d73c9a57f7defc59fc184d8129
Fela Winkelmolen <felawinkelmolen@gmail.com>
2013-03-13 21:47:06 +0100

    game generation works
    I did many changes and quite forgot about git commits... (:
    
    Changes include:
    
    * Element renominated to Cell
    * some refactoring
    * creation of mazes (no unique solution checked yet)

--- netconnect.js
+++ netconnect.js
@@ -10,66 +10,109 @@ function get_random_color() {
     return color;
 }
 
+function randomDirection() {
+    return Math.floor(Math.random()*4);
+}
+
+Array.prototype.random = function() {
+    return this[Math.floor(Math.random() * this.length)];
+}
+
 var game = {};
 game.context = context;
 game.init = function() {
-    this.width = 400;
-    this.height = 400;
-    this.rows = 4;
-    this.cols = 4;
-    this.elements = [];
-    size = this.width / this.rows;
-    for (r = 0; r < this.rows; ++r) {
-        for (c = 0; c < this.cols; ++c) {
-            e = new Element(r, c, size);
-            this.elements.push(e);
-        }
-    }
     
     canvas.onclick = function(evt) {
         var res = getMousePosition(canvas, evt);
         x = res[0];
         y = res[1];
         
-        var row = Math.floor(x / (game.width/game.rows));
-        var col = Math.floor(y / (game.width/game.cols));
-        var elem = game.elementAt(row, col);
+        var row = Math.floor(y / (game.width/game.cols));
+        var col = Math.floor(x / (game.width/game.rows));
+        var cell = game.cellAt(row, col);
         
         var clockwise = Math.floor(x / (game.width/game.rows/2))%2 == 1;
         
         
         if (clockwise) {
-            elem.rotateClockwise();
+            cell.rotateClockwise();
         } else {
-            elem.rotateCounterClockwise();
+            cell.rotateCounterClockwise();
         }
         game.draw();
 
     }
     
-    this.elementAt = function(row, col) {
+    this.cellAt = function(row, col) {
         var i = row * this.cols + col;
-        return this.elements[i];
+        return this.cells[i];
     }
     
     this.draw = function() {
-        for (var i = 0; i < this.elements.length; ++i) {
-            this.elements[i].draw();
+        for (var i = 0; i < this.cells.length; ++i) {
+            this.cells[i].draw();
         }
     }
     
+    this.createGame = function() {
+        // create empty cells
+        var size = this.width / this.rows;
+        for (var r = 0; r < this.rows; ++r) {
+            for (var c = 0; c < this.cols; ++c) {
+                var e = new Cell(r, c, size, this);
+                this.cells.push(e);
+            }
+        }
+        
+        // cells to which cables can be added
+        // empty cells are not present
+        // (except for the first random empty position)
+        // calls get added as soon as they get a cable
+        var initialCell = this.cells.random();
+        initialCell.neighborsCannotConnect();
+        var incompleteCells = [initialCell];
+        var emptyCells = this.rows * this.cols;
+        while (emptyCells > 0 && incompleteCells.length > 0) {
+            var cell = incompleteCells.random();
+            var newCell = cell.addRandomCable();
+            incompleteCells.push(newCell);
+            // remove complete cells
+            var newIncompleteCells = []
+            for (var i = 0; i < incompleteCells.length; ++i) {
+                if (!incompleteCells[i].isComplete()) {
+                    newIncompleteCells.push(incompleteCells[i]);
+                }
+            }
+            incompleteCells = newIncompleteCells;
+            
+            // debugging only
+            cell.dirty = true;
+            newCell.dirty = true;
+            cell.draw();
+            newCell.draw();
+            
+            emptyCells--;
+        }
+    }
     
+    this.width = 400;
+    this.height = 400;
+    this.rows = 4;
+    this.cols = 4;
+    this.cells = [];
+    this.createGame();
     this.draw();
+    
 }
 
 
 ///////////////////////////////////////////////////////////////////////////////
-///////////////////////            Element           //////////////////////////
+///////////////////////             Cell             //////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 // Represents a square
 
 
-function Element(row, col, size) {
+function Cell(row, col, size, game) {
     // testing function, returns mostly false but sometimes true
     this.maybe = function() {
         if (Math.random() > 0.2) {
@@ -78,14 +121,83 @@ function Element(row, col, size) {
         return true;
     };
     // default values, might be overridden
-    this.x = size*row;
-    this.y = size*col;
+    this.row = row;
+    this.col = col;
+    this.x = size*col;
+    this.y = size*row;
     this.width = size;
     this.height = size;
     this.background = get_random_color();
+    this.game = game;
     // wether there is a cable up right down and left, respectively
+    this.Up = 0
+    this.Right = 1
+    this.Down = 2
+    this.Left = 3
     this.cables = [false,false,false,false];
-    this.cables = [this.maybe(),this.maybe(),this.maybe(),this.maybe()]
+    
+    // for game creation
+    // wether a new cable could be added up, right, down, left, respertively
+    this.canAddCable = [true, true, true, true];
+    if (this.row == 0) {
+        this.canAddCable[this.Up] = false;
+    }
+    if (this.col == 0) {
+        this.canAddCable[this.Left] = false;
+    }
+    if (this.row == game.rows-1) {
+        this.canAddCable[this.Down] = false;
+    }
+    if (this.col == game.cols-1) {
+        this.canAddCable[this.Right] = false;
+    }
+    
+    this.addRandomCable = function() {
+        var validDirection = false;
+        var direction = undefined;
+        if (this.isComplete()) {
+            alert('cannot add cable to complete cell!!!');
+        }
+        while (!validDirection) {
+            direction = randomDirection();
+            validDirection = this.canAddCable[direction];
+        }
+        this.cables[direction] = true;
+        this.canAddCable[direction] = false;
+        var oppositeDirection = (direction + 2) % 4;
+        var n = this.neighbor(direction);
+        n.cables[oppositeDirection] = true;
+        n.canAddCable[oppositeDirection] = false;
+        // to avoid loops avoid possibility to connect to the
+        // newly connected cell
+        n.neighborsCannotConnect();
+        return n;
+    }
+    
+    this.neighborsCannotConnect = function() {
+        for (var dir = 0; dir < 4; ++dir) {
+            if (this.neighbor(dir)) {
+                var oppositeDir = (dir + 2) % 4;
+                this.neighbor(dir).canAddCable[oppositeDir] = false;
+            }
+        }
+    }
+    this.isComplete = function() {
+        return this.canAddCable[0] == false &&
+                this.canAddCable[1] == false &&
+                this.canAddCable[2] == false &&
+                this.canAddCable[3] == false
+        /*for (el in this.canAddCable) {
+            if (el) {
+                // can add a cable
+                return false;
+            }
+        }
+        // cannot add any cable
+        return true;*/
+    }
+    
+    
     this.context = game.context;
     this.dirty = true;
     this.draw = function(){
@@ -160,7 +272,22 @@ function Element(row, col, size) {
         this.cables.push(first);
         this.dirty = true;
     }
+    this.neighbor = function(direction) {
+        switch (direction) {
+            case this.Up:
+                return this.game.cellAt(row-1, col);
+            case this.Left:
+                return this.game.cellAt(row, col-1);
+            case this.Down:
+                return this.game.cellAt(row+1, col);
+            case this.Right:
+                return this.game.cellAt(row, col+1);
+            default:
+                return undefined;
+        }
+    }
 }
+
 
 function getMousePosition(canvas, event) {
     var element = canvas;
