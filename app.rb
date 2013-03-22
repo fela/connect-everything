c2084899233457f9fed2b93ce48af21fa88b7765
Fela Winkelmolen <felawinkelmolen@gmail.com>
2013-03-22 11:37:06 +0100

    use datamapper and connect to pg

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require 'sinatra'
+require 'data_mapper'
+ 
+class Item
+  include DataMapper::Resource
+ 
+  property :text, String,:key => true
+end
+ 
+configure do
+  DataMapper.setup(:default, 'postgres://fela:@localhost/net-connect')
+  DataMapper.finalize
+  DataMapper.auto_migrate!
+ 
+  Item.create(:text => "hi")
+end
 
 get '/' do
   send_file 'public/index.html'
