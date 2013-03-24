5c8dda10cbecea541beacc85b077bfef603e62b1
Fela Winkelmolen <felawinkelmolen@gmail.com>
2013-03-24 20:11:38 +0100

    penalize moves more

--- public/netconnect.js
+++ public/netconnect.js
@@ -202,10 +202,10 @@ game.init = function() {
         var timeScore = 1 / Math.sqrt(minutes);
         
         var movePenalty = -this.moves/2;
-        var moveScore = Math.pow(2/3, Math.sqrt(movePenalty));
+        var moveScore = Math.pow(2/3, Math.pow(movePenalty, 0.7));
         
         // combine the the scores
-        var multFactor = 1/4;
+        var multFactor = 1/2;
         var score = multFactor * difficultyScore * timeScore * moveScore;
         // the square root is to decrease unneded differences between low and high scores
         return Math.sqrt(score);
