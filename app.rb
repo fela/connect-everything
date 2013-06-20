bafbe000982a45a597704d163b647bf4996104c5
Fela Winkelmolen <felawinkelmolen@gmail.com>
2013-06-20 21:44:07 +0200

    reenable more...

--- app.rb
+++ app.rb
@@ -152,7 +152,7 @@ helpers do
   def show_hiscores
     @overAllChart = Score.chart(single_entries: true)
     @recentChart = Score.recent_chart
-    @recentPeopleChart = []#Score.recent_players
+    @recentPeopleChart = Score.recent_players
     haml :hiscores
   end
   
